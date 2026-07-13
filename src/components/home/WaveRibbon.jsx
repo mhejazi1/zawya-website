152cedd96e6f6aaa9ce2efc8ff072601c7eae036
mhejazi1 <mhejazi1@icloud.com>
2026-07-13 06:24:10 +0000

    Visual edits

--- src/components/home/WaveRibbon.jsx
+++ src/components/home/WaveRibbon.jsx
@@ -1,7 +1,7 @@
 export default function WaveRibbon() {
-  return (
-    <div className="relative h-8 w-full overflow-hidden hidden">
-      <div className="absolute inset-0 wave-gradient hidden" />
-    </div>);
+  return null;
+
+
+
 
 }
