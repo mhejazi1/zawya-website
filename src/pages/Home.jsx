6711b879acca45ff8bfe1264452054fbf332fcce
mhejazi1 <82715402+mhejazi1@users.noreply.github.com>
2026-07-23 02:00:49 +0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import HeroSection from "@/components/home/HeroSection";
 import StatsSection from "@/components/home/StatsSection";
 import ServicesOverview from "@/components/home/ServicesOverview";
@@ -6,6 +7,11 @@ import PartnerBar from "@/components/home/PartnerBar";
 import ClosingCTA from "@/components/home/ClosingCTA";
 
 export default function Home() {
+  useEffect(() => {
+    document.documentElement.classList.add("no-scrollbar");
+    return () => document.documentElement.classList.remove("no-scrollbar");
+  }, []);
+
   return (
     <>
       <HeroSection />
